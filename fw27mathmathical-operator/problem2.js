6c1fae29d285df0ab37db7b6ddfba7298c59c864
VIVEK <shsrna41@gmail.com>
2023-04-15 13:33:00 +0530

    second day 2

---
+++ fw27mathmathical-operator/problem2.js
@@ -0,0 +1,12 @@
+console.log(7%5);
+console.log(10%11);
+console.log(1%454154752);
+console.log(14%4);
+
+// let a=15;
+// let a=25;
+// console.log(a);
+
+var a=15;
+var a=25;
+console.log(a);
---
+++ fw27mathmathical-operator/problem3.js
@@ -0,0 +1,10 @@
+let name="Vivek Kumar Sharma";
+let school="IPEC Ghaziabad";
+let grade=8;
+let section="G";
+let rollNumber=1602100;
+let marksMath=65;
+let marksPhysics=68;
+let marksChemistry=70;
+console.log(name,school,grade,section,rollNumber,marksMath,marksPhysics,marksChemistry);
+
