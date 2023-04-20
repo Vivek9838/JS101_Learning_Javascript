17ad744b30fe7ba34c48a9c12a1d7121867b63e7
VIVEK <shsrna41@gmail.com>
2023-04-20 10:36:23 +0530

    for loop

---
+++ forLoop/index.js
@@ -0,0 +1,29 @@
+// let num = 8;
+// let i = 1;
+// while (i <= num) {
+//   if 
+//      ((i%10==0))    {
+//     console.log(i);
+
+//   }
+//   ++i;
+// }
+
+// let a="";
+// for(let i=1;i<=10;i++){
+//   a=a+i;
+//   console.log(a)
+// }
+
+// let a="";
+// for(let i=1;i<=8;i++){
+//   a=a+i+"*";
+// }
+// console.log(a);
+
+//product of number;
+ let a=1;
+ for(let i=1;i<=5;i++){
+   a=a*i;
+ }
+ console.log(a);
