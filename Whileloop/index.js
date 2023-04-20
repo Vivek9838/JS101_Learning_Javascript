022dbe0f4107fa634493c5d6a619f61f251937a0
VIVEK <shsrna41@gmail.com>
2023-04-20 10:33:58 +0530

    while loop

---
+++ Whileloop/index.js
@@ -0,0 +1,61 @@
+// let a=1;
+
+// while(a<=10){
+//   console.log(a);
+//   a++;
+// }
+
+// let a=5;
+// while(a>0){
+//   console.log(a);
+//   a--;
+// }
+
+// find Even  number 
+
+// let a=0;
+// while(a<=5){
+//   if(a%2==0){
+//     console.log(a);
+//   }
+//   a++;
+// }
+
+//  // Find  Odd value 
+ // let a=0;
+ // while(a<=15){
+ //   if(!(a%2==0)){
+ //     console.log(a);
+ //   }
+ //   a++;
+ // }
+
+// let a=0;
+// let sum=0;
+// while(a<=6){
+//   if(a%3==0){
+//     // console.log(a);
+//     sum=sum+a;
+//   }
+//   a++;
+// }
+// console.log(sum);
+// let N=5;
+// let a=1;
+//     let sum=0;
+//     while(a<=N){
+//         sum=sum+a;
+       
+//         ++a;
+//     }
+//  console.log(sum);
+// let num=7;
+//  let i=1;
+//     while(i<=num){
+//         if(i%10){
+//             console.log(i);
+//         }
+//         ++i;
+//     }
+
+//Average number
