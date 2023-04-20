71c104ba1a9e567a6d20f0f356066d019017ec9b
VIVEK <shsrna41@gmail.com>
2023-04-20 11:07:26 +0530

    string concept

---
+++ fw27string/index.js
@@ -0,0 +1,67 @@
+//Every character print in new line;
+// let a="xnjsxjhsacnacascbdsn";
+// for(let i=0;i<a.length;i++){
+//   console.log(a[i]);
+// }
+
+//how many spaces found
+// let space=0;
+// let a="masai school in everyone";
+// for(let i=0;i<a.length;i++){
+//   if(a[i]==" "){
+//     space++;
+//   }
+// }
+// console.log(space);
+
+//
+
+
+//find the how many "v"
+// let a=["vivk","vv","vkfj","ufnf","vks"]
+// let count=0;
+// for(let i=0;i<a.length;i++){
+//   if(a[i][0]=="v"){
+//     count++;
+//   }
+// }
+// console.log(count);
+
+
+//reverse the string
+// let a="masai";
+// let b=" ";
+// for(let i=a.length-1;i>=0;i--){
+//   b=b+a[i];
+  
+// }
+// console.log(b)
+
+//home work part;
+
+//googel ----> oogelg;
+//  let str="google";
+// let final=" ";
+// for(let i=(str.length/2)-1;i>=0;i--){
+//   final=final+str[i];
+// }
+// // console.log(final);
+
+// for(let i=str.length-1;i>=str.length/2;i--){
+//   final=final+str[i];
+// }
+// console.log(final);
+
+// let A=[2,3,5,7,1,8,10,14];
+// for(let i=A.length-1;i>=0;i--){
+//   if(A[i]%2==1){
+//       console.log(A[i]);
+//   }
+
+// }
+// let A=[14,17,18,18,20,22,26]
+//  for(let a=A.length-1;a>=0;a--){
+//         if(A[a]%2==0){
+//             console.log(A[a]);
+//         }
+//     }
