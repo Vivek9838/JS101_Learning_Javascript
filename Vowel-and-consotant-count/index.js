79ffea1f82a0cd6f6bece987222c8469aa74c8e9
VIVEK <shsrna41@gmail.com>
2023-04-25 21:02:57 +0530

    count vowel and consonant

---
+++ Vowel-and-consotant-count/index.js
@@ -0,0 +1,60 @@
+//  let count=0;
+// let count1=0;
+//   let  str="vivek"
+//   for(let i=0;i<str.length;i++){
+
+//         if(str[i]=="a" || str[i]=="e" || str[i]=="i"  || str[i]=="o" || str[i]=="u"){
+//           count++;
+
+//       } else if(str[i]!="a" || str[i]!="e" || str[i]!="i"  || str[i]!="o" || str[i]!="u") {
+//         count1++;
+//       } 
+
+
+//   }
+
+// console.log(count,count1);
+//  let sum=0;
+//   let sum1=0;
+// let arr=n;
+// let n=5;
+//  for(let i=1;i<=n;i++){
+//          if((i%2)==0){
+//             sum=sum+arr[i];
+//              console.log(sum);
+//         }
+//          else if((i%2)!=0){
+//        sum1=sum1+arr[i];
+//        console.log(sum1);
+//        }
+//        else if(sum>sum1){
+//         console.log("Even");
+//     }
+
+//     }
+
+// let array=[1,2,3,4,5]
+// let K=1;
+//  let count=0;
+// for(let i=0; i<array.length;i++){
+
+//       if(array[i]%2==0){
+//           count++;
+//       // }if(count>K){
+//       //     console.log("Nice Array");
+//       // }else{
+//       //     console.log("Bad Array");
+//       // }
+
+//   }
+//   console.log(count);
+// }
+
+let a = "136";
+if (a[2] % 3 == 0) {
+  console.log("yes");
+
+}
+else {
+  console.log("No");
+}
